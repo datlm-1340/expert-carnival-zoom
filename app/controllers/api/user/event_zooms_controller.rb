0fe6f031f412e965e20db987290b37da5bb4f76a
le.manh.dat <le.manh.dat@sun-asterisk.com>
2022-06-10 09:49:42 +0700

    Seperate BE and FE

--- app/controllers/api/user/event_zooms_controller.rb
+++ app/controllers/api/user/event_zooms_controller.rb
@@ -14,13 +14,41 @@ class Api::User::EventZoomsController < Api::BaseController
   end
 
   def show
-    event_zoom = EventZoom.find(params[:id]).as_json(
-      only: EVENT_ZOOM_ATTRS,
-      methods: EVENT_ZOOM_METHODS
-    )
+    event_zoom = EventZoom.find_by(id: params[:id])
 
-    render_json(event_zoom, :ok)
-  rescue
-    render_json({}, :not_found)
+    return render_json(event_zoom, :ok) unless event_zoom
+
+    result = {
+      id: event_zoom.id,
+      metting_id: event_zoom.metting_id,
+      metting_name: event_zoom.metting_name,
+      metting_pw: event_zoom.metting_pw,
+      start_url: event_zoom.start_url,
+      url: event_zoom.url,
+      signature: get_signature(event_zoom),
+      user_email: current_user.email,
+      user_name: "[#{current_user.role}] #{current_user.email}"
+    }
+
+    render_json(result, :ok)
+  end
+
+  private
+
+  def get_signature event_zoom
+    iat = Time.now.to_i - 30;
+    exp = iat + 60 * 60 * 2
+    header = { alg: 'HS256', typ: 'JWT' }
+    payload = {
+      sdkKey: Settings.zoom.sdk_key,
+      mn: event_zoom.metting_id,
+      role: current_user.user? ? 0 : 1,
+      iat: iat,
+      exp: exp,
+      appKey: Settings.zoom.sdk_key,
+      tokenExp: exp
+    }
+
+    JWT.encode(payload, Settings.zoom.sdk_secret, 'HS256', header)
   end
 end
--- app/models/event_zoom.rb
+++ app/models/event_zoom.rb
@@ -1,18 +1,2 @@
 class EventZoom < ApplicationRecord
-  def signature
-    iat = Time.now.to_i - 30;
-    exp = iat + 60 * 60 * 2
-    header = { alg: 'HS256', typ: 'JWT' }
-    payload = {
-      sdkKey: Settings.zoom.sdk_key,
-      mn: metting_id,
-      role: 0, # chỉ cho PIC làm host
-      iat: iat,
-      exp: exp,
-      appKey: Settings.zoom.sdk_key,
-      tokenExp: iat + 60 * 60 * 2
-    }
-
-    JWT.encode(payload, Settings.zoom.sdk_secret, 'HS256', header)
-  end
 end
